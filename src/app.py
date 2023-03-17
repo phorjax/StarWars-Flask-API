20729646717742a093b042791881a008c2b56f9d
phorjax <joseph.12082@gmail.com>
2023-03-17 03:34:29 +0000

    new

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planet, People
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,96 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+# all the people
+@app.route('/people', methods=['GET'])
+def get_all_people():
+    users = People.query.all()
+    all_people = list(map(lambda x: x.serialize(), users))
+    return jsonify(all_people), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
 
+# id of person
+@app.route('/people/<int:user_id>', methods=['GET'])
+def get_people_id(user_id):
+    response_body = request.get_json()
+    user = People.query.get(user_id)
+    return jsonify(user), 200
+
+
+# all planets
+@app.route('/planets', methods=['GET'])
+def get_planets_id():
+    users = Planet.query.all()
+    all_people = list(map(lambda x: x.serialize(), users))
+    return jsonify(all_people), 200
+
+
+# id of planets
+@app.route('/planets/<int:user_id>', methods=['GET'])
+def get_all_planets(user_id):
+    response_body = request.get_json()
+    user = Planet.query.get(user_id)
+    return jsonify(user), 200
+
+
+
+# all blog users
+@app.route('/users', methods=['GET'])
+def get_all_users():
+    users = User.query.all()
+    all_people = list(map(lambda x: x.serialize(), users))
+    return jsonify(all_people), 200
+
+
+
+
+
+@app.route('/user', methods=['POST'])
+def handle_user():
+    response_body = request.get_json()
+    user = User(email=response_body["email"], password=response_body["password"])
+    db.session.add(user)
+    db.session.commit()
     return jsonify(response_body), 200
+
+
+
+
+
+
+
+@app.route('/user/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    response_body = request.get_json()
+    user = User.query.get(user_id)
+    
+    if user is None:
+        raise APIException("user not found", 404)
+    if "email" in response_body:
+        user.email = response_body["email"]
+    if "password" in response_body:
+        user.password = response_body["password"]
+    db.session.commit()
+    return jsonify(response_body), 200
+
+
+
+
+
+
+
+
+@app.route('/user/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    user = User.query.get(user_id)
+    if user is None:
+        raise APIException("user not found", 404)
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify("has been deleted"), 200
+
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, ForeignKey, Integer, String
 
 db = SQLAlchemy()
 
@@ -6,14 +7,74 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
+    is_active = db.Column(db.Boolean(), unique=False, nullable=True, default=True)
+    planets_id = db.relationship('Planet', lazy=True)
+    people_id = db.relationship('People', lazy=True)
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            # "planets": [Planet.serialize() for planet in self.planets],
+            # "people": [People.serialize() for person in self.people]
             # do not serialize the password, its a security breach
         }
+    
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    mass = db.Column(db.Integer, unique=False, nullable=True)
+    diameter = db.Column(db.Integer, unique=False, nullable=True)
+    gravity = db.Column(db.Integer, unique=False, nullable=True)
+    orbital_period = db.Column(db.Integer, unique=False, nullable=True)
+    climate = db.Column(db.String(80), unique=False, nullable=True)
+    terrain = db.Column(db.String(80), unique=False, nullable=True)
+    favorite_planets = db.Column(db.Integer, db.ForeignKey(User.id))
+
+    def __repr__(self):
+        return '<Planet %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "mass": self.mass,
+            # "diameter": self.diameter,
+            # "gravity": self.gravity,
+            # "orbital_period": self.orbital_period,
+            # "climate": self.climate,
+            # "terrain": self.terrain,
+        }
+        
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    gender = db.Column(db.String(80), unique=False, nullable=True)
+    height = db.Column(Integer, unique=False, nullable=True)
+    weight = db.Column(Integer, unique=False, nullable=True)
+    age = db.Column(Integer, unique=False, nullable=True)
+    race = db.Column(db.String(80), unique=False, nullable=True)
+    hair_color = db.Column(db.String(80), unique=False, nullable=True)
+    eye_color = db.Column(db.String(80), unique=False, nullable=True)
+    favorite_people = db.Column(db.Integer, db.ForeignKey(User.id))
+
+
+    def __repr__(self):
+        return '<People %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            # "gender": self.gender,
+            # "height": self.height,
+            # "weight": self.weight,
+            # "age": self.age,
+            # "race": self.race,
+            # "hair_color": self.hair_color,
+            # "eye_color": self.eye_color,            
+        }
+        
